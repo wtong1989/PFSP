688f53b9421f05dbbe402ae6a7f08bf5fc5611bd
smbct <buchet.s@laposte.net>
2017-03-22 09:58:07 +0100

    tests done!

--- tests/script.sh
+++ tests/script.sh
@@ -1,23 +1,48 @@
+#!/bin/bash
+
+function save {
+
+    str=$(cat output)
+    printf "$str\n"
+
+    best=$(printf "$str" | grep -o -E 'Best: [-+0-9.e]+' | cut -d ' ' -f2 )
+    t=$(printf "$str" | grep -o -E 'Time: [-+0-9.e]+' | cut -d ' ' -f2 )
+
+    printf "$1 $2 $3 $4 $best $t\n" >> res.txt
+
+}
+
+function run {
+
+    for i in ${pivot[@]}
+    do
+        for j in ${init[@]}
+        do
+            for k in ${neigh[@]}
+            do
+                echo $i $j $k
+                ./../code/flowshopWCT $1 $i $j $k > output
+
+                a=$(printf "$1" | grep -o -E 'instances/.*' |cut -d '/' -f2)
+
+                save $a $i $j $k
+            done
+        done
+    done
+
+}
+
 declare -a pivot=("--firstImp" "--bestImp")
 declare -a init=("--srz" "--random")
 declare -a neigh=("--transpose" "--exchange" "--insert")
 
-#for f in ../instances/*
-#do
-#    printf $f
-#    printf '\n'
-#done
+rm res.txt
 
-#echo ${pivot[1]}
-
-for i in ${pivot[@]}
+for f in ../instances/*
 do
-    for j in ${init[@]}
-    do
-        for k in ${neigh[@]}
-        do
-            echo $i $j $k
-            ./../code/flowshopWCT ../instances/50_20_01 $i $j $k > output
-        done
-    done
+   printf $f
+   run $f
 done
+
+
+rm output
